e9806b0c6dc0eefb8d1eca3eea4aa8897f6e4bd6
ed.a.ezekiel@gmail.com <ed.a.ezekiel@gmail.com>
2018-09-08 16:34:20 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var recipes = { firstMeal: "breakfast"}
+
+function updateObjectWithKeyAndValue(obj, key, value) {
+ return Object.assign({}, obj, { [key]: value})}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+	obj[key] = value
+	return obj
+}
+
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj.key;
+  newObj
+}
+
+
+
+function destructivelyDeleteFromObjectByKey(object, key){
+
+}
